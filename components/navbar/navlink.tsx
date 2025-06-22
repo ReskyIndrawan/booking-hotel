8d9edf6a27c0bb1504d3e2ed743d52a682c0c403
Resky Indrawan <indrawanresky.tlg@gmail.com>
2025-06-23 04:29:11 +0900

    feat: setup database with Neon, add Prisma & NextAuth with role-based auth
    - Configure PostgreSQL database using Vercel Neon
    - Install and setup Prisma, Prisma Client, Prisma Adapter, and NextAuth
    - Create and configure authjs.d.ts for custom user types (role)
    - Implement authentication middleware and API route for /api/auth
    - Add role-based route protection for secure access control

--- components/navbar/navlink.tsx
+++ components/navbar/navlink.tsx
@@ -3,13 +3,36 @@
 import Link from "next/link";
 import { useState } from "react";
 import { IoClose, IoMenu } from "react-icons/io5";
+import { useSession, signOut } from "next-auth/react";
+import Image from "next/image";
 import clsx from "clsx";
 
 const Navlink = () => {
   const [open, setOpen] = useState(false);
+  const { data: session } = useSession();
 
   return (
     <>
+      {session?.user ? (
+        <div className='flex items-center  md:order-2 gap-2 '>
+          <div className='hidden md:block md:me-0 bg-white/70 hover:scale-110 hover:bg-teal-400 rounded-full p-0.5'>
+            <Image
+              src={session.user.image || "/avatar.svg"}
+              width={40}
+              height={40}
+              alt='avatar'
+              className='rounded-full border-0 object-cover'
+            />
+          </div>
+          <div className='flex items-center'>
+            <button
+              onClick={() => signOut()}
+              className='md:block hidden py-2 px-4 bg-blue-400 text-white font-semibold hover:bg-blue-500 rounded-sm cursor-pointer'>
+              Sign Out
+            </button>
+          </div>
+        </div>
+      ) : null}
       <button
         onClick={() => setOpen(!open)}
         className='inline-flex items-center p-2 justify-between text-sm text-white rounded-md md:hidden hover:bg-gray-700'>
@@ -52,34 +75,52 @@ const Navlink = () => {
               Contact
             </Link>
           </li>
-          <li>
-            <Link
-              href='/myreservation'
-              className='block py-2 px-3 text-white hover:bg-teal-500 rounded-sm md:hover:bg-transparent md:p-0 md:hover:text-teal-400'>
-              My Reservation
-            </Link>
-          </li>
-          <li>
-            <Link
-              href='/admin/dashboard'
-              className='block py-2 px-3 text-white hover:bg-teal-500 rounded-sm md:hover:bg-transparent md:p-0 md:hover:text-teal-400'>
-              Dashboard
-            </Link>
-          </li>
-          <li>
-            <Link
-              href='/admin/room'
-              className='block py-2 px-3 text-white hover:bg-teal-500 rounded-sm md:hover:bg-transparent md:p-0 md:hover:text-teal-400'>
-              Manage Room
-            </Link>
-          </li>
-          <li className='pt-4 md:pt-0'>
-            <Link
-              href='/signin'
-              className='py-2.5 px-6 bg-teal-500 text-white hover:bg-teal-600 rounded-sm border-1 border-black shadow-lg'>
-              Sign In
-            </Link>
-          </li>
+          {session && (
+            <>
+              <li>
+                <Link
+                  href='/myreservation'
+                  className='block py-2 px-3 text-white hover:bg-teal-500 rounded-sm md:hover:bg-transparent md:p-0 md:hover:text-teal-400'>
+                  My Reservation
+                </Link>
+              </li>
+              {session.user.role === "admin" && (
+                <>
+                  <li>
+                    <Link
+                      href='/admin/dashboard'
+                      className='block py-2 px-3 text-white hover:bg-teal-500 rounded-sm md:hover:bg-transparent md:p-0 md:hover:text-teal-400'>
+                      Dashboard
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      href='/admin/room'
+                      className='block py-2 px-3 text-white hover:bg-teal-500 rounded-sm md:hover:bg-transparent md:p-0 md:hover:text-teal-400'>
+                      Manage Room
+                    </Link>
+                  </li>
+                </>
+              )}
+            </>
+          )}
+          {session ? (
+            <li className='pt-4 md:pt-0'>
+              <button
+                onClick={() => signOut()}
+                className=' md:hidden py-2.5 px-4 bg-blue-500 text-white hover:bg-blue-600 rounded-sm border-1 border-black shadow-lg cursor-pointer'>
+                Sign Out
+              </button>
+            </li>
+          ) : (
+            <li className='pt-4 md:pt-0'>
+              <Link
+                href='/signin'
+                className='py-2.5 px-6 bg-teal-500 text-white hover:bg-teal-600 rounded-sm border-1 border-black shadow-lg'>
+                Sign In
+              </Link>
+            </li>
+          )}
         </ul>
       </div>
     </>
